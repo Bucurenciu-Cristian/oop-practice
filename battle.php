34b61f70f7b540cc0efd0ad02130891c0d50714f
cristian-bucurenciu <cristian.bucurenciu@sepa.at>
2022-01-16 11:45:42 +0200

    Service Classes

--- battle.php
+++ battle.php
@@ -25,8 +25,9 @@ if ($ship1Quantity <= 0 || $ship2Quantity <= 0) {
 
 $ship1 = $ships[$ship1Name];
 $ship2 = $ships[$ship2Name];
+$battleManager = new BattleManager();
 
-$outcome = battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
+$outcome = $battleManager->battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
 ?>
 
 <html>
