f1febcec41f2d4c2cbd4a64ad2003fcb42ccce8e
Bucurenciu Bogdan <bucurenciu.cristian@gmail.com>
2022-01-27 12:22:39 +0200

    Episode No. 3 - Protected

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -17,4 +17,24 @@ class RebelShip extends Ship
     {
         return 'Rebel';
     }
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s (REBEL)',
+                $this->getName(),
+                $this->getWeaponPower(),
+                $this->getJediFactor(),
+                $this->getStrength()
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s (REBEL)',
+                $this->getName(),
+                $this->getWeaponPower(),
+                $this->getJediFactor(),
+                $this->getStrength()
+            );
+        }
+    }
 }
